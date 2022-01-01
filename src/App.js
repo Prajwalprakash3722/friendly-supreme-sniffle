e35c0834f2ec4f4cfd838254410e561ffa4fb9c4
Prajwal P <prajwalprakash3722@gmail.com>
2022-01-02 00:15:10 +0530

    [initial commit]

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-3xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
